f6b302c806404253833f3ea3ad53d78b7a4f6873
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-08-20 19:49:19 +0300

    add test in class AfishaManagerTest

--- src/test/java/ru/netology/AfishaManagerTest.java
+++ src/test/java/ru/netology/AfishaManagerTest.java
@@ -5,33 +5,34 @@ import org.junit.jupiter.api.Test;
 import ru.netology.repository.AfishaRepository;
 import ru.netology.manager.AfishaManager;
 
+
 public class AfishaManagerTest {
-    AfishaManager afishaManager = new AfishaManager(3);
-    AfishaRepository afishaRepository1 = new AfishaRepository(1, "Bloodshort", "Action");
-    AfishaRepository afishaRepository2 = new AfishaRepository(2, "Forward", "Cartoon");
-    AfishaRepository afishaRepository3 = new AfishaRepository(3, "Belgrad Hotel", "Comedy");
-    AfishaRepository afishaRepository4 = new AfishaRepository(4, "Gentlemens", "Action");
-    AfishaRepository afishaRepository5 = new AfishaRepository(5, "Invisible Man", "Thriller");
-    AfishaRepository afishaRepository6 = new AfishaRepository(6, "Trolls world tour", "Cartoon");
-    AfishaRepository afishaRepository7 = new AfishaRepository(7, "Number One", "Comedy");
+    AfishaManager afishaManager = new AfishaManager( 3 );
+    AfishaRepository afishaRepository1 = new AfishaRepository( 1, "Bloodshort", "Action" );
+    AfishaRepository afishaRepository2 = new AfishaRepository( 2, "Forward", "Cartoon" );
+    AfishaRepository afishaRepository3 = new AfishaRepository( 3, "Belgrad Hotel", "Comedy" );
+    AfishaRepository afishaRepository4 = new AfishaRepository( 4, "Gentlemens", "Action" );
+    AfishaRepository afishaRepository5 = new AfishaRepository( 5, "Invisible Man", "Thriller" );
+    AfishaRepository afishaRepository6 = new AfishaRepository( 6, "Trolls world tour", "Cartoon" );
+    AfishaRepository afishaRepository7 = new AfishaRepository( 7, "Number One", "Comedy" );
 
     @Test
     void shouldAdd() {
         AfishaRepository[] expected = new AfishaRepository[]{afishaRepository1};
         afishaManager.add( afishaRepository1 );
-        Assertions.assertArrayEquals(expected, afishaManager.getMovie());
-    }
-    @Test
-    void shouldReturnLessThenMax () {
-        AfishaRepository[] expected = new AfishaRepository[] {afishaRepository7, afishaRepository6, afishaRepository5};
-        afishaManager.add ( afishaRepository1 );
-        afishaManager.add ( afishaRepository2 );
-        afishaManager.add ( afishaRepository3 );
-        afishaManager.add ( afishaRepository4 );
-        afishaManager.add ( afishaRepository5 );
-        afishaManager.add ( afishaRepository6 );
-        afishaManager.add ( afishaRepository7 );
-        Assertions.assertArrayEquals(expected, afishaManager.getMovie());
+        Assertions.assertArrayEquals( expected, afishaManager.getMovie() );
     }
 
+    @Test
+    void shouldReturnLessThenMax() {
+        AfishaRepository[] expected = new AfishaRepository[]{afishaRepository7, afishaRepository6, afishaRepository5};
+        afishaManager.add( afishaRepository1 );
+        afishaManager.add( afishaRepository2 );
+        afishaManager.add( afishaRepository3 );
+        afishaManager.add( afishaRepository4 );
+        afishaManager.add( afishaRepository5 );
+        afishaManager.add( afishaRepository6 );
+        afishaManager.add( afishaRepository7 );
+        Assertions.assertArrayEquals( expected, afishaManager.getMovie() );
+    }
 }
